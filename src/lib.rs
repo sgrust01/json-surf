758bf95425a89256b05ec0031b507356a1f54c7c
Saurav Gupta <saurav2k4u@gmail.com>
2020-05-25 15:08:17 +0800

    Updated documentation, added example and bumped crate version

--- src/lib.rs
+++ src/lib.rs
@@ -89,7 +89,7 @@
 //!     let body = "He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty-four days now without taking a fish.".to_string();
 //!     let old_man = OldMan::new(title, body);
 //!
-//!     // Insert the data so that store as only one document
+//!     // Insert the data so that store has only one document
 //!     let _ = surfer.insert_struct(&name, &old_man).unwrap();
 //!     println!("Inserting document: 1");
 //!
@@ -103,11 +103,11 @@
 //!     println!("Total documents found: {}", computed.len());
 //!     assert_eq!(computed, vec![old_man.clone()]);
 //!
-//!     // Insert the data so that store as two document
+//!     // Insert the data so that store has two document
 //!     let _ = surfer.insert_struct(&name, &old_man).unwrap();
 //!     println!("Inserting document: 1");
 //!
-//!     // Give some time to indexing to complete
+//!     // Give some time for indexing to complete
 //!     block_thread(1);
 //!
 //!     // Lets query again for two documents
@@ -117,7 +117,7 @@
 //!     println!("Total documents found: {}", computed.len());
 //!     assert_eq!(computed, vec![old_man.clone(), old_man.clone()]);
 //!
-//!     // Lets add 100 more documents
+//!     // Lets add 50 more documents
 //!     let mut i = 0;
 //!     let mut documents = Vec::with_capacity(50);
 //!     while i < 50 {
@@ -127,20 +127,20 @@
 //!     let _ = surfer.insert_structs(&name, &documents).unwrap();
 //!     println!("Inserting document: 50");
 //!
-//!     // Give some time to indexing to complete
+//!     // Give some time for indexing to complete
 //!     block_thread(2);
 //!
-//!     // Lets query again for to get first 10 only
+//!     // Lets query again to get first 10 documents only (default settings)
 //!     let query = "sea whale";
 //!     let computed = surfer.read_structs::<OldMan>(&name, query, None, None).unwrap().unwrap();
 //!     // Check 10 documents
 //!     println!("Total documents found: {} due to default limit = 10", computed.len());
 //!     assert_eq!(computed.len(), 10);
 //!
-//!     // Lets query again for to get first 10 only
+//!     // Lets query again to get all stored documents
 //!     let query = "sea whale";
 //!     let computed = surfer.read_structs::<OldMan>(&name, query, Some(100), None).unwrap().unwrap();
-//!     // Check 10 documents
+//!     // Check all 52 documents
 //!     println!("Total documents found: {} with limit = 100", computed.len());
 //!     assert_eq!(computed.len(), 52);
 //!
