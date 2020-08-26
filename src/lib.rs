b0262e91120a35de9317365f8f9e286244d63e09
Saurav Gupta <saurav2k4u@gmail.com>
2020-08-26 23:36:40 +0800

    Examples clean-up

--- src/lib.rs
+++ src/lib.rs
@@ -33,122 +33,126 @@
 //! ```rust
 //! use std::convert::TryFrom;
 //! use std::fs::remove_dir_all;
+//! use std::cmp::{Ord, Ordering, Eq};
 //!
 //! use serde::{Serialize, Deserialize};
 //!
 //! use json_surf::prelude::*;
 //!
-//! /// Document to be indexed and searched
-//! #[derive(Serialize, Debug, Deserialize, PartialEq, Clone)]
-//! struct OldMan {
-//!     title: String,
-//!     body: String,
+//! /// Main struct to save
+//! #[derive(Serialize, Debug, Deserialize, PartialEq, PartialOrd, Clone)]
+//! struct UserInfo {
+//!     first: String,
+//!     last: String,
+//!     age: u8,
 //! }
 //!
-//! impl OldMan {
-//!     pub fn new(title: String, body: String) -> Self {
+//! impl UserInfo {
+//!     pub fn new(first: String, last: String, age: u8) -> Self {
 //!         Self {
-//!             title,
-//!             body,
+//!             first,
+//!             last,
+//!             age,
 //!         }
 //!     }
 //! }
 //!
-//! /// Convenience implementation for bootstraping the builder
-//! impl Default for OldMan {
+//! impl Default for UserInfo {
 //!     fn default() -> Self {
-//!         let title = "".to_string();
-//!         let body = "".to_string();
-//!         OldMan::new(title, body)
+//!         let first = "".to_string();
+//!         let last = "".to_string();
+//!         let age = 0u8;
+//!         UserInfo::new(first, last, age)
 //!     }
 //! }
 //!
-//! /// Convenience method to keep indexes tucked under a directory
-//! fn home_and_random_index_name() -> (String, String) {
-//!     let home = ".store/examples".to_string();
-//!     let name = random_string(None);
-//!     (home, name)
-//! }
 //!
 //! fn main() {
-//!     let (home, name) = home_and_random_index_name();
-//!
-//!     // Mostly empty but can be a real flat struct
-//!     let data = OldMan::default();
-//!
-//!     // Prepare the builder instance
+//!     let home = ".store".to_string();
+//!     let name = "users".to_string();
 //!     let mut builder = SurferBuilder::default();
-//!     // By default everything goes to directory indexes
 //!     builder.set_home(&home);
+//!
+//!     let data = UserInfo::default();
 //!     builder.add_struct(name.clone(), &data);
 //!
-//!     // Make the Surfer
 //!     let mut surfer = Surfer::try_from(builder).unwrap();
 //!
-//!     // Prepare your data or get it from somewhere
-//!     let title = "The Old Man and the Sea".to_string();
-//!     let body = "He was an old man who fished alone in a skiff in the Gulf Stream and he had gone eighty-four days now without taking a fish.".to_string();
-//!     let old_man = OldMan::new(title, body);
+//!     let first = "John".to_string();
+//!     let last = "Doe".to_string();
+//!     let age = 20u8;
+//!     let john_doe = UserInfo::new(first, last, age);
 //!
-//!     // Insert the data so that store has only one document
-//!     let _ = surfer.insert_struct(&name, &old_man).unwrap();
-//!     println!("Inserting document: 1");
+//!     let first = "Jane".to_string();
+//!     let last = "Doe".to_string();
+//!     let age = 18u8;
+//!     let jane_doe = UserInfo::new(first, last, age);
 //!
-//!     // Give some time to indexing to complete
-//!     block_thread(1);
+//!     let users = vec![john_doe.clone(), jane_doe.clone()];
+//!     let _ = surfer.insert_structs(&name, &users).unwrap();
+//!     println!("===========================");
+//!     println!("Insert: John & Jane Doe");
+//!     println!("---------------------------");
+//!     println!("{:#?}", users);
+//!     println!("---------------------------");
 //!
-//!     // Lets query our one document
-//!     let query = "sea whale";
-//!     let computed = surfer.read_structs::<OldMan>(&name, query, None, None).unwrap().unwrap();
-//!     // Check one document
-//!     println!("Total documents found: {}", computed.len());
-//!     assert_eq!(computed, vec![old_man.clone()]);
 //!
-//!     // Insert the data so that store has two document
-//!     let _ = surfer.insert_struct(&name, &old_man).unwrap();
-//!     println!("Inserting document: 1");
+//!     println!("===========================");
+//!     println!("Search users with Age = 20");
+//!     println!("---------------------------");
+//!     let query = "20";
+//!     let mut computed = surfer.read_stucts_by_field::<UserInfo>(&name, "age", query, Some(100), None).unwrap().unwrap();
+//!     computed.sort();
+//!     let mut expected = vec![john_doe];
+//!     expected.sort();
+//!     assert_eq!(computed, expected);
+//!     println!("{:#?}", computed);
+//!     println!("---------------------------");
 //!
-//!     // Give some time for indexing to complete
-//!     block_thread(1);
 //!
-//!     // Lets query again for two documents
-//!     let query = "sea whale";
-//!     let computed = surfer.read_structs::<OldMan>(&name, query, None, None).unwrap().unwrap();
-//!     // Check two documents
-//!     println!("Total documents found: {}", computed.len());
-//!     assert_eq!(computed, vec![old_man.clone(), old_man.clone()]);
-//!
-//!     // Lets add 50 more documents
-//!     let mut i = 0;
-//!     let mut documents = Vec::with_capacity(50);
-//!     while i < 50 {
-//!         documents.push(old_man.clone());
-//!         i = i + 1;
-//!     };
-//!     let _ = surfer.insert_structs(&name, &documents).unwrap();
-//!     println!("Inserting document: 50");
-//!
-//!     // Give some time for indexing to complete
-//!     block_thread(2);
-//!
-//!     // Lets query again to get first 10 documents only (default settings)
-//!     let query = "sea whale";
-//!     let computed = surfer.read_structs::<OldMan>(&name, query, None, None).unwrap().unwrap();
-//!     // Check 10 documents
-//!     println!("Total documents found: {} due to default limit = 10", computed.len());
-//!     assert_eq!(computed.len(), 10);
-//!
-//!     // Lets query again to get all stored documents
-//!     let query = "sea whale";
-//!     let computed = surfer.read_structs::<OldMan>(&name, query, Some(100), None).unwrap().unwrap();
-//!     // Check all 52 documents
-//!     println!("Total documents found: {} with limit = 100", computed.len());
-//!     assert_eq!(computed.len(), 52);
+//!     println!("===========================");
+//!     println!("Search users with Age = 18");
+//!     println!("---------------------------");
+//!     let query = "18";
+//!     let mut computed = surfer.read_stucts_by_field::<UserInfo>(&name, "age", query, Some(100), None).unwrap().unwrap();
+//!     computed.sort();
+//!     let mut expected = vec![jane_doe];
+//!     expected.sort();
+//!     assert_eq!(computed, expected);
+//!     println!("{:#?}", computed);
+//!     println!("---------------------------");
 //!
 //!     // Clean-up
+//!
 //!     let path = surfer.which_index(&name).unwrap();
-//!     let _ = remove_dir_all(&path);
+//!     println!("Serving queries from {}", path);
+//!     let _ = remove_dir_all(&home);
 //! }
+//!
+//! /// Convenience method for sorting & likely not required in user code
+//! impl Ord for UserInfo {
+//!     fn cmp(&self, other: &Self) -> Ordering {
+//!         if self.first == other.first && self.last == other.last {
+//!             return Ordering::Equal;
+//!         };
+//!         if self.first == other.first {
+//!             if self.last > self.last {
+//!                 Ordering::Greater
+//!             } else {
+//!                 Ordering::Less
+//!             }
+//!         } else {
+//!             if self.first > self.first {
+//!                 Ordering::Greater
+//!             } else {
+//!                 Ordering::Less
+//!             }
+//!         }
+//!     }
+//! }
+//!
+//! /// Convenience method for sorting & likely not required in user code
+//! impl Eq for UserInfo {}
 //! ```
 
 pub mod prelude;
